a82d800befcebde515718eaac4a9d65645d0370f
Jordan <acey1jordan@gmail.com>
2021-12-19 11:18:01 -0700

    trying a different secret

--- src/Components/Checkout/pages/Checkout.js
+++ src/Components/Checkout/pages/Checkout.js
@@ -1,8 +1,5 @@
 import React, { Component } from "react";
-// import { Link } from 'react-router-dom'
 import StripeContainer from './StripeContainer';
-// import { Elements } from '@stripe/react-stripe-js';
-import { PaymentForm } from '@stripe/react-stripe-js';
 import { connect }from 'react-redux';
 ;
 class Checkout extends Component {
