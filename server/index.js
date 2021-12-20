8768d1c67d2e0bd597ae8940e71e215901ee583c
Jordan <acey1jordan@gmail.com>
2021-12-20 12:23:20 -0700

    hosting

--- server/index.js
+++ server/index.js
@@ -34,16 +34,16 @@ massive({
      console.log('Datbase Connecton Error', err)
 })
 
-if (process.env.NODE_ENV === 'production') {
-    // Exprees will serve up production assets
-    app.use(express.static('client/build'));
+// if (process.env.NODE_ENV === 'production') {
+//     // Exprees will serve up production assets
+//     app.use(express.static('client/build'));
   
-    // Express serve up index.html file if it doesn't recognize route
-    const path = require('path');
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
+//     // Express serve up index.html file if it doesn't recognize route
+//     const path = require('path');
+//     app.get('*', (req, res) => {
+//       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//     });
+//   }
 
 
 app.use(express.static(`${__dirname}/../build`));
