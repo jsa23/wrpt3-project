ec0403439d6a447ca01aa646237a8186b62977b7
Jordan <acey1jordan@gmail.com>
2021-12-26 17:28:44 -0700

    Final One

--- src/Components/Checkout/pages/PaymentForm.js
+++ src/Components/Checkout/pages/PaymentForm.js
@@ -40,7 +40,7 @@ export default function PaymentForm(props) {
     if(!error) {
         try {
             const {id} = paymentMethod
-            const response = await axios.post("http://localhost:4532/payment", {
+            const response = await axios.post("http://localhost:5432/payment", {
                 amount: props.total,
                 id
             })
